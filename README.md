a2c377dead3e9fe7e1fe22602459568969b9343c
leandecks <leandrogridelli@gmail.com>
2022-07-04 15:19:09 +0200

    v1.0.0 Piece of code finished

---
+++ README.md
@@ -0,0 +1,3 @@
+## theme-switcher
+
+Unleashed Design's [theme-switch](https://www.youtube.com/watch?v=3IoGET2jkk4) tested/copied by Leandecks with custom themes (light, dark, neon).
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
 import './../sass/style.sass'
+
+const buttons = document.querySelectorAll('.switch');
+const darkmode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+if (darkmode) {
+  document.body.dataset.theme = "dark";
+} else {
+  document.body.dataset.theme = "";
+}
+
+buttons.forEach(button => {
+  button.addEventListener('click', setTheme);
+})
+
+function setTheme($event) {
+  document.body.dataset.theme = $event.target.dataset.theme;
+}
